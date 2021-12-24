a62513e910dcf3c0d8d2a07b991ceb8bfe1b934d
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-12-23 20:30:31 -0500

    borre las extensiones de client: client_name, etc

--- src/models/sale-detail.model.js
+++ src/models/sale-detail.model.js
@@ -2,25 +2,22 @@
 const { Schema, model } = require("mongoose");
 
 const SaleDetailSchema = new Schema(
-    {
-        name_artist_song: { type: String, required: true },
-        tag: { type: Schema.Types.ObjectId, ref: "Tag" },
-        sale: { type: Schema.Types.ObjectId, ref: "Sale" },
-        subtotal: { type: Number, required: true },
-        amount: { type: Number, required: true },
-        client: { type: Schema.Types.ObjectId, ref: "Client", required: false },
-        client_name: { type: String, required: true },
-        client_lastname: { type: String, required: true },
-        client_email: { type: String, required: true },
-        createdAt: { type: Date, default: Date.now, required: true },
-    },
-    { collection: "sale-details" }
+  {
+    name_artist_song: { type: String, required: true },
+    tag: { type: Schema.Types.ObjectId, ref: "Tag" },
+    sale: { type: Schema.Types.ObjectId, ref: "Sale" },
+    subtotal: { type: Number, required: true },
+    amount: { type: Number, required: true },
+    client: { type: Schema.Types.ObjectId, ref: "Client", required: false },
+    createdAt: { type: Date, default: Date.now, required: true },
+  },
+  { collection: "sale-details" }
 );
 
 SaleDetailSchema.method("toJSON", function () {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
 });
 
 module.exports = model("SaleDetail", SaleDetailSchema);
--- src/models/sale.model.js
+++ src/models/sale.model.js
@@ -2,28 +2,25 @@
 const { Schema, model } = require("mongoose");
 
 const SaleSchema = new Schema(
-    {
-        client: { type: Schema.Types.ObjectId, ref: "Client", required: false },
-        client_name: { type: String, required: true },
-        client_lastname: { type: String, required: true },
-        client_email: { type: String, required: true },
-        nsale: { type: String, required: true },
-        subtotal: { type: Number, required: false },
-        total: { type: Number, required: true },
-        igv: { type: Number, required: true },
-        transaction: { type: String, required: true },
-        state: { type: String, default: "Pendiente", required: true },
-        phone: { type: String, required: false },
-        report: { type: String, required: false },
-        createdAt: { type: Date, default: Date.now, required: true },
-    },
-    { collection: "sales" }
+  {
+    client: { type: Schema.Types.ObjectId, ref: "Client", required: false },
+    nsale: { type: String, required: true },
+    subtotal: { type: Number, required: false },
+    total: { type: Number, required: true },
+    igv: { type: Number, required: true },
+    transaction: { type: String, required: true },
+    state: { type: String, default: "Pendiente", required: true },
+    phone: { type: String, required: false },
+    report: { type: String, required: false },
+    createdAt: { type: Date, default: Date.now, required: true },
+  },
+  { collection: "sales" }
 );
 
 SaleSchema.method("toJSON", function () {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
 });
 
 module.exports = model("Sale", SaleSchema);
